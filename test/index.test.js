5631f45b43d8012125fde89eec678feb5f445d0b
exuanbo <sambercheng@gmail.com>
2020-03-30 23:56:47 +0200

    fix: rename wrong class  `icons`

--- test/index.test.js
+++ test/index.test.js
@@ -3,45 +3,45 @@ const icons = require('../index.js')
 
 describe('test file name with extention "index.js"', () => {
   test('get class string', () => {
-    expect(icons.getClass('index.js')).toBe('icons js-icon medium-yellow')
+    expect(icons.getClass('index.js')).toBe('icon js-icon medium-yellow')
   })
   test('get class string without color', () => {
-    expect(icons.getClass('index.js', { color: false })).toBe('icons js-icon')
+    expect(icons.getClass('index.js', { color: false })).toBe('icon js-icon')
   })
   test('get class array', () => {
-    expect(icons.getClass('index.js', { array: true })).toEqual(['icons', 'js-icon', 'medium-yellow'])
+    expect(icons.getClass('index.js', { array: true })).toEqual(['icon', 'js-icon', 'medium-yellow'])
   })
   test('get class array without color', () => {
-    expect(icons.getClass('index.js', { color: false, array: true })).toEqual(['icons', 'js-icon'])
+    expect(icons.getClass('index.js', { color: false, array: true })).toEqual(['icon', 'js-icon'])
   })
 })
 
 describe('test directory name "node_modules"', () => {
   test('get class string', () => {
-    expect(icons.getClass('node_modules')).toBe('icons node-icon medium-green')
+    expect(icons.getClass('node_modules')).toBe('icon node-icon medium-green')
   })
   test('get class string without color', () => {
-    expect(icons.getClass('node_modules', { color: false })).toBe('icons node-icon')
+    expect(icons.getClass('node_modules', { color: false })).toBe('icon node-icon')
   })
   test('get class array', () => {
-    expect(icons.getClass('node_modules', { array: true })).toEqual(['icons', 'node-icon', 'medium-green'])
+    expect(icons.getClass('node_modules', { array: true })).toEqual(['icon', 'node-icon', 'medium-green'])
   })
   test('get class array without color', () => {
-    expect(icons.getClass('node_modules', { color: false, array: true })).toEqual(['icons', 'node-icon'])
+    expect(icons.getClass('node_modules', { color: false, array: true })).toEqual(['icon', 'node-icon'])
   })
 })
 
 describe('test language name "typescript"', () => {
   test('get class string', () => {
-    expect(icons.getClass('typescript')).toBe('icons ts-icon medium-blue')
+    expect(icons.getClass('typescript')).toBe('icon ts-icon medium-blue')
   })
   test('get class string without color', () => {
-    expect(icons.getClass('typescript', { color: false })).toBe('icons ts-icon')
+    expect(icons.getClass('typescript', { color: false })).toBe('icon ts-icon')
   })
   test('get class array', () => {
-    expect(icons.getClass('typescript', { array: true })).toEqual(['icons', 'ts-icon', 'medium-blue'])
+    expect(icons.getClass('typescript', { array: true })).toEqual(['icon', 'ts-icon', 'medium-blue'])
   })
   test('get class array without color', () => {
-    expect(icons.getClass('typescript', { color: false, array: true })).toEqual(['icons', 'ts-icon'])
+    expect(icons.getClass('typescript', { color: false, array: true })).toEqual(['icon', 'ts-icon'])
   })
 })
