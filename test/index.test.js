4d248afd11e22991ce550b7a040324d66eeef7c0
exuanbo <sambercheng@gmail.com>
2020-03-31 12:55:44 +0200

    test: add tests for better coverage

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 const icons = require('../index.js')
 
-describe('test file name with extention "index.js"', () => {
+describe('matchName file "index.js"', () => {
   test('get class string', () => {
     expect(icons.getClass('index.js')).toBe('icon js-icon medium-yellow')
   })
@@ -16,7 +16,7 @@ describe('test file name with extention "index.js"', () => {
   })
 })
 
-describe('test directory name "node_modules"', () => {
+describe('matchName directory "node_modules"', () => {
   test('get class string', () => {
     expect(icons.getClass('node_modules')).toBe('icon node-icon medium-green')
   })
@@ -31,7 +31,7 @@ describe('test directory name "node_modules"', () => {
   })
 })
 
-describe('test language name "typescript"', () => {
+describe('matchLanguage "typescript"', () => {
   test('get class string', () => {
     expect(icons.getClass('typescript')).toBe('icon ts-icon medium-blue')
   })
@@ -43,5 +43,50 @@ describe('test language name "typescript"', () => {
   })
   test('get class array without color', () => {
     expect(icons.getClass('typescript', { color: false, array: true })).toEqual(['icon', 'ts-icon'])
+  })
+})
+
+describe('matchScope "source.js"', () => {
+  test('get class string', () => {
+    expect(icons.getClass('source.js')).toBe('icon js-icon medium-yellow')
+  })
+  test('get class string without color', () => {
+    expect(icons.getClass('source.js', { color: false })).toBe('icon js-icon')
+  })
+  test('get class array', () => {
+    expect(icons.getClass('source.js', { array: true })).toEqual(['icon', 'js-icon', 'medium-yellow'])
+  })
+  test('get class array without color', () => {
+    expect(icons.getClass('source.js', { color: false, array: true })).toEqual(['icon', 'js-icon'])
+  })
+})
+
+describe('matchInterprater "zsh"', () => {
+  test('get class string', () => {
+    expect(icons.getClass('zsh')).toBe('icon terminal-icon medium-purple')
+  })
+  test('get class string without color', () => {
+    expect(icons.getClass('zsh', { color: false })).toBe('icon terminal-icon')
+  })
+  test('get class array', () => {
+    expect(icons.getClass('zsh', { array: true })).toEqual(['icon', 'terminal-icon', 'medium-purple'])
+  })
+  test('get class array without color', () => {
+    expect(icons.getClass('zsh', { color: false, array: true })).toEqual(['icon', 'terminal-icon'])
+  })
+})
+
+describe('matchSignature "\x1F\x8B"', () => {
+  test('get class string', () => {
+    expect(icons.getClass('\x1F\x8B')).toBe('icon zip-icon dark-blue')
+  })
+  test('get class string without color', () => {
+    expect(icons.getClass('\x1F\x8B', { color: false })).toBe('icon zip-icon')
+  })
+  test('get class array', () => {
+    expect(icons.getClass('\x1F\x8B', { array: true })).toEqual(['icon', 'zip-icon', 'dark-blue'])
+  })
+  test('get class array without color', () => {
+    expect(icons.getClass('\x1F\x8B', { color: false, array: true })).toEqual(['icon', 'zip-icon'])
   })
 })
