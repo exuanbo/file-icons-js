be782c32d3fb52ec574f20d804958acdf58ae31c
exuanbo <sambercheng@gmail.com>
2020-04-03 22:06:31 +0200

    feat: add css with web font

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const source = require('vinyl-source-stream')
 const streamify = require('gulp-streamify')
 const uglify = require('gulp-uglify-es').default
 const path = require('path')
+const replace = require('gulp-replace')
 const less = require('gulp-less')
 const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
@@ -26,15 +27,20 @@ function js() {
 
 function css() {
   return src(['styles/icons.less'])
-    .pipe(
-      less({
-        paths: [path.join(__dirname, 'styles')]
-      })
-    )
+    .pipe(less({ paths: [path.join(__dirname, 'styles')] }))
     .pipe(rename('file-icons.css'))
-    .pipe(dest('dist'))
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('dist'))
+    .pipe(
+      replace(
+        /\.\./g,
+        'https://cdn.jsdelivr.net/npm/@exuanbo/file-icons-js@latest'
+      )
+    )
+    .pipe(
+      rename({ basename: 'file-icons', suffix: '-cdn', extname: '.min.css' })
+    )
     .pipe(dest('dist'))
 }
 
